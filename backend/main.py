92beab91576f13a5f1b3e28f9f4c260cb8f77980
treethought <cpsweene@gmail.com>
2016-10-17 14:41:33 -0700

    removed old js files

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         sleep(0.1)
 
     logger.debug("Server started")
-    webview.create_window("My first pywebview application", "http://127.0.0.1:23948", min_size=(640, 480))
+    webview.create_window("sp-404", "http://127.0.0.1:23948", min_size=(640, 480))
 
     # webview.create_window("My first pywebview application", "http://www.treethought.com", min_size=(640, 480))
 
