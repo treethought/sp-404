e9a9fa0cffc8c02f6774d306df1c8cc4bcad6911
treethought <cpsweene@gmail.com>
2016-10-17 14:23:02 -0700

    removed olf src dir

--- src/backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-APP_ROOT = os.path.dirname(os.path.dirname(os.path.dirname((__file__))))
-GUI = os.path.join(APP_ROOT, 'static')
-IMAGES = os.path.join(GUI, 'images')
-SAMPLES = os.path.join(GUI, 'wav_files')
-
